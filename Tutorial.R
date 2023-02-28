17e5e727eb657b45148368a2eb1772fd11cb8700
hycheon123 <jeon@iastate.edu>
2023-02-28 17:19:04 -0500

    Update Tutorial.R

--- Tutorial.R
+++ Tutorial.R
@@ -9,6 +9,7 @@ if(length(new.packages)) install.packages(new.packages)
 
 # Sources:
 source("All_sources.R")
+nominal.alpha = 0.01
 
 ###############################################################################################################
 
@@ -22,12 +23,12 @@ m = nrow(data); pi0_orr = orr.estimate.m0(data$pA, data$pB, B = 20)/m
 
 # Step 2-1. alpha selection.
 set.seed(1)
-rep_alphas = rep.alpha_0501(p1 = data$pA, p2 = data$pB, alphas = 0.05, rep_per = 0.05, M.mc = 100)
+rep_alphas = rep.alpha_0501(p1 = data$pA, p2 = data$pB, alphas = nominal.alpha, rep_per = 0.05, M.mc = 10000)
 
 # Step 2-2. DEG selection. # output = readRDS(paste0("extra/M_10000_alpha",0.05)); output$adj_alpha
-                         # When M.mc = 10000, rep_alphas = 0.04255349.
+# When M.mc = 10000, rep_alphas = 0.04255349.
 
-DEG.proposed = Inference_R(p1 = data$pA, p2 = data$pB, epi0 = pi0_orr, alpha = rep_alphas)$DEGs
+DEG.proposed = Inference_R(p1 = data$pA, p2 = data$pB, epi0 = pi0_orr, alpha = rep_alphas)
 
 
 ###############################################################################################################
